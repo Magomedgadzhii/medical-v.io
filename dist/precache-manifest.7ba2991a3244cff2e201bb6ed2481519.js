562262bb1e815550c84aa5b986877380175e9615
Magomedgadzhii <108467549+Magomedgadzhii@users.noreply.github.com>
2022-09-01 23:49:39 +0300

    Add files via upload

---
+++ dist/precache-manifest.7ba2991a3244cff2e201bb6ed2481519.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24473c392c02dfec3e8",
+    "url": "/css/about.2eb1e8dd.css"
+  },
+  {
+    "revision": "3ba2e8b2545f57ca6c2f",
+    "url": "/css/app.a11faf8b.css"
+  },
+  {
+    "revision": "8bcf42284a0623c2390e8d27eb04304d",
+    "url": "/img/111111.8bcf4228.jpg"
+  },
+  {
+    "revision": "343064440a14969b0626ae6fb25278d5",
+    "url": "/img/chekap.34306444.jpg"
+  },
+  {
+    "revision": "b33f79b1ef7e1eacb09cbaef77067d0f",
+    "url": "/img/doctor.b33f79b1.png"
+  },
+  {
+    "revision": "5d2a9a17f7b3b2f3abf7ca8030de3d28",
+    "url": "/img/doctors2.5d2a9a17.png"
+  },
+  {
+    "revision": "031c2efff1eb740f1563645063717f7a",
+    "url": "/img/gospital3.031c2eff.png"
+  },
+  {
+    "revision": "340f6e9bae887881e0b47087fac70acb",
+    "url": "/img/heartbeat.340f6e9b.png"
+  },
+  {
+    "revision": "8adf52942d129f467758d9f5a5264fa6",
+    "url": "/img/hotel.8adf5294.png"
+  },
+  {
+    "revision": "adcfae50e8b98dfd4175ccd73bbb02af",
+    "url": "/img/med-obsledovanie.adcfae50.jpg"
+  },
+  {
+    "revision": "c31c016c37b1708798f86936d4531bdf",
+    "url": "/img/medical-history.c31c016c.png"
+  },
+  {
+    "revision": "917cd47b07dbb0833402a238af4d2f5d",
+    "url": "/img/medical-report.917cd47b.png"
+  },
+  {
+    "revision": "05c2bc8375b1796d8bcaef4b0c182387",
+    "url": "/img/medsy.05c2bc83.jpeg"
+  },
+  {
+    "revision": "c4e4ba35a34f830861ee764369efb3e0",
+    "url": "/img/neotlojka.c4e4ba35.jpg"
+  },
+  {
+    "revision": "e7cf9a48c1993cf884ad72ab477a1810",
+    "url": "/img/offer4.e7cf9a48.png"
+  },
+  {
+    "revision": "100bdd688a675590ee0991068c8b2591",
+    "url": "/img/pacient1.100bdd68.png"
+  },
+  {
+    "revision": "46fe3c39de59df46a3e605653cfa6a27",
+    "url": "/img/reobeletacia.46fe3c39.jpg"
+  },
+  {
+    "revision": "dc330f2b6dab706ed2f14d525740fd68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d24473c392c02dfec3e8",
+    "url": "/js/about.20b8eb39.js"
+  },
+  {
+    "revision": "3ba2e8b2545f57ca6c2f",
+    "url": "/js/app.c9bfeeaa.js"
+  },
+  {
+    "revision": "75bbc21dff31e0bc56bd",
+    "url": "/js/chunk-vendors.5ccef800.js"
+  },
+  {
+    "revision": "f401c814e2be201a849be2eb4bb3f45a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
